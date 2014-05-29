c73c85ceeb25e6a31a3d2be62c1130a9168f3d0f
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-05-29 18:22:47 +0200

    started working on unicode case folding

---
+++ llhd/unicode.cpp
@@ -0,0 +1,9 @@
+/* Copyright (c) 2014 Fabian Schuiki */
+#include "llhd/unicode.hpp"
+
+const char* llhd::unicode::fullCaseFold(const char* c) {
+}
+
+const char* llhd::unicode::simpleCaseFold(const char* c) {
+}
+
---
+++ llhd/unicode.hpp
@@ -0,0 +1,11 @@
+/* Copyright (c) 2014 Fabian Schuiki */
+#pragma once
+
+namespace llhd {
+namespace unicode {
+
+const char* fullCaseFold(const char* c);
+const char* simpleCaseFold(const char* c);
+
+} // namespace unicode
+} // namespace llhd
