7783da6b93bfa4905558923332a3724f33a96e35
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-06-15 11:41:28 +0200

    cleanup of LexerNovum and Token code
    Added \todo doxygen instructions to LexerNovum.cpp. Also cleaned up things
    there and started using the new NullTerminatedIterator. Cleaned up the Token
    implementation.

--- llhd/vhdl/LexerNovum.cpp
+++ llhd/vhdl/LexerNovum.cpp
@@ -1,8 +1,8 @@
 /* Copyright (c) 2014 Fabian Schuiki */
-#include "llhd/SourceBuffer.hpp"
 #include "llhd/unicode.hpp"
+#include "llhd/NullTerminatedIterator.hpp"
+#include "llhd/SourceBuffer.hpp"
 #include "llhd/vhdl/keywords.hpp"
-#include "llhd/vhdl/KeywordMapper.hpp"
 #include "llhd/vhdl/LexerNovum.hpp"
 #include "llhd/vhdl/TokenContext.hpp"
 #include "llhd/vhdl/Token.hpp"
@@ -11,134 +11,28 @@
 #include <iostream>
 using namespace llhd::vhdl;
 
-// inline bool isWhitespace(const char* c) {
-// 	return *c == ' ' || *c == '\t' || *c == '\n'
-// }
-
 /// Implemented according to IEEE 1076-2000.
 void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
-	const utf8char* bc = src.getStart();
-	const utf8char* c = bc;
+	// const utf8char* bc = src.getStart();
+	// const utf8char* c = bc;
+
+	NullTerminatedIterator<utf8char> bc(src.getStart(), src.getEnd());
+	NullTerminatedIterator<utf8char> c = bc;
 
 	auto emit = [this,&bc,&c,&loc] (unsigned type) {
 		if (bc == c)
 			return;
 		SourceLocation nloc = loc + (c-bc);
 		if (type > 0) {
-			// Token* tkn = this->ctx.alloc.one<Token>(
-			// 	SourceBuffer(bc,c),
-			// 	SourceRange(loc,nloc),
-			// 	type);
-			// this->ctx.addToken(tkn);
-			std::cout << "emitting " << std::hex << type << std::dec << " " << std::string(bc,c) << "\n";
-			std::cout.flush();
+			Token* tkn = this->ctx.alloc.one<Token>(
+				SourceRange(loc,nloc),
+				type);
+			this->ctx.addToken(tkn);
+			// std::cout << "emitting " << std::hex << type << std::dec << " " << std::string(bc,c) << "\n";
 		}
 		bc = c;
 		loc = nloc;
 	};
-
-	KeywordMapper<> keywords; keywords
-		("abs", kKeywordAbs)
-		("access", kKeywordAccess)
-		("after", kKeywordAfter)
-		("alias", kKeywordAlias)
-		("all", kKeywordAll)
-		("and", kKeywordAnd)
-		("architecture", kKeywordArchitecture)
-		("array", kKeywordArray)
-		("assert", kKeywordAssert)
-		("attribute", kKeywordAttribute)
-		("begin", kKeywordBegin)
-		("block", kKeywordBlock)
-		("body", kKeywordBody)
-		("buffer", kKeywordBuffer)
-		("bus", kKeywordBus)
-		("case", kKeywordCase)
-		("component", kKeywordComponent)
-		("configuration", kKeywordConfiguration)
-		("constant", kKeywordConstant)
-		("label", kKeywordLabel)
-		("disconnect", kKeywordDisconnect)
-		("downto", kKeywordDownto)
-		("map", kKeywordMap)
-		("else", kKeywordElse)
-		("elsif", kKeywordElsif)
-		("end", kKeywordEnd)
-		("entity", kKeywordEntity)
-		("exit", kKeywordExit)
-		("file", kKeywordFile)
-		("for", kKeywordFor)
-		("function", kKeywordFunction)
-		("generate", kKeywordGenerate)
-		("generic", kKeywordGeneric)
-		("group", kKeywordGroup)
-		("guarded", kKeywordGuarded)
-		("if", kKeywordIf)
-		("impure", kKeywordImpure)
-		("in", kKeywordIn)
-		("inertial", kKeywordInertial)
-		("inout", kKeywordInout)
-		("is", kKeywordIs)
-		("library", kKeywordLibrary)
-		("linkage", kKeywordLinkage)
-		("literal", kKeywordLiteral)
-		("loop", kKeywordLoop)
-		("mod", kKeywordMod)
-		("nand", kKeywordNand)
-		("new", kKeywordNew)
-		("next", kKeywordNext)
-		("nor", kKeywordNor)
-		("not", kKeywordNot)
-		("null", kKeywordNull)
-		("of", kKeywordOf)
-		("on", kKeywordOn)
-		("open", kKeywordOpen)
-		("or", kKeywordOr)
-		("others", kKeywordOthers)
-		("out", kKeywordOut)
-		("package", kKeywordPackage)
-		("port", kKeywordPort)
-		("postponed", kKeywordPostponed)
-		("procedural", kKeywordProcedural)
-		("procedure", kKeywordProcedure)
-		("process", kKeywordProcess)
-		("protected", kKeywordProtected)
-		("pure", kKeywordPure)
-		("range", kKeywordRange)
-		("record", kKeywordRecord)
-		("reference", kKeywordReference)
-		("register", kKeywordRegister)
-		("reject", kKeywordReject)
-		("rem", kKeywordRem)
-		("report", kKeywordReport)
-		("return", kKeywordReturn)
-		("rol", kKeywordRol)
-		("ror", kKeywordRor)
-		("select", kKeywordSelect)
-		("severity", kKeywordSeverity)
-		("shared", kKeywordShared)
-		("signal", kKeywordSignal)
-		("sla", kKeywordSla)
-		("sll", kKeywordSll)
-		("sra", kKeywordSra)
-		("srl", kKeywordSrl)
-		("subtype", kKeywordSubtype)
-		("then", kKeywordThen)
-		("to", kKeywordTo)
-		("transport", kKeywordTransport)
-		("type", kKeywordType)
-		("unaffected", kKeywordUnaffected)
-		("units", kKeywordUnits)
-		("until", kKeywordUntil)
-		("use", kKeywordUse)
-		("variable", kKeywordVariable)
-		("wait", kKeywordWait)
-		("when", kKeywordWhen)
-		("while", kKeywordWhile)
-		("with", kKeywordWith)
-		("xnor", kKeywordXnor)
-		("xor", kKeywordXor)
-		.compile();
 
 	while (*c != 0) {
 		// Characters 0x01..0x20 are treated as whitespace. This range covers
@@ -154,9 +48,8 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 				if (*c == 0xa0) c++;
 			}
 			emit(skipWhitespaces ? kTokenInvalid : kTokenWhitespace);
-			// TODO: Add pedantic mode which emits warnings if the the
-			// character read is not strictly a whitespace as defined by the
-			// standard.
+			/// \todo Add pedantic mode which emits warnings if the character
+			/// read is not strictly a whitespace as defined by the standard.
 		}
 
 		// Comments start with a double hyphen and proceed until the end of the
@@ -219,14 +112,14 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 				if (*c == '\\') c++; // allow \\ escaping
 				c++;
 
-				// TODO: Add pedantic mode which checks whether the characters
-				// actually fall into VHDL's "191 graphic characters". By
-				// default the parser is forgiving and allows virtually every
-				// character here.
+				/// \todo Add pedantic mode which checks whether the characters
+				/// actually fall into VHDL's "191 graphic characters". By
+				/// default the parser is forgiving and allows virtually every
+				/// character here.
 			}
 			if (*c != '\\') {
-				// TODO: Emit error indicating that the extended identifier is
-				// not terminated and abort.
+				/// \todo Emit error indicating that the extended identifier is
+				/// not terminated and abort.
 			}
 			c++;
 			emit(kTokenExtendedIdentifier);
@@ -259,14 +152,14 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 			c++; // consume the character
 			while (*c & 0x80) c++; // consume longer unicode characters
 			if (*c != '\'') {
-				// TODO: emit error indicating that the character literal
-				// contains more than one character. Fast-forward to the
-				// next apostrophe to suggest a fix.
+				/// \todo emit error indicating that the character literal
+				/// contains more than one character. Fast-forward to the
+				/// next apostrophe to suggest a fix.
 			}
 			c++; // consume apostrophe
-			// TODO: Add pedantic mode which checks whether the encapsulated
-			// character actually is among the "191 graphic characters"
-			// mentioned in §13.5.
+			/// \todo Add pedantic mode which checks whether the encapsulated
+			/// character actually is among the "191 graphic characters"
+			/// mentioned in §13.5.
 			emit(kTokenCharacterLiteral);
 		}
 
@@ -284,19 +177,19 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 				if (*c == end) c++; // allow %% and "" escaping
 				if (*c == '\\') {
 					c++; // tolerate backspace escaping
-					// TODO: Add pedantic mode which would emit an error here
-					// indicating that backspace escaping is not part of the
-					// standard.
+					/// \todo Add pedantic mode which would emit an error here
+					/// indicating that backspace escaping is not part of the
+					/// standard.
 				}
 				c++;
 			}
 			if (*c != end) {
-				// TODO: Emit error indicating that the string literal is not
-				// terminated and abort.
+				/// \todo Emit error indicating that the string literal is not
+				/// terminated and abort.
 			}
 			c++; // consume terminating character
-			// TODO: Add pedantic mode which checks whether the encapsulated
-			// string actually contains only the allowed graphical characters.
+			/// \todo Add pedantic mode which checks whether the encapsulated
+			/// string actually contains only the allowed graphical characters.
 			emit(kTokenStringLiteral);
 		}
 
@@ -318,15 +211,15 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 				if (*c == end) c++; // allow %% and "" escaping
 				if (*c == '\\') {
 					c++; // tolerate backspace escaping
-					// TODO: Add pedantic mode which would emit an error here
-					// indicating that backspace scaping is not part of the
-					// standard.
+					/// \todo Add pedantic mode which would emit an error here
+					/// indicating that backspace scaping is not part of the
+					/// standard.
 				}
 				c++;
 			}
 			if (*c != end) {
-				// TODO: Emit error indicating that the bit string literal is
-				// not terminated and abort.
+				/// \todo Emit error indicating that the bit string literal is
+				/// not terminated and abort.
 			}
 			c++; // consume terminating character.
 			emit(kTokenBitStringLiteral);
@@ -349,13 +242,13 @@ void LexerNovum::lex(const SourceBuffer& src, SourceLocation loc) {
 			       ((*c & 0x80) && !(*c == 0xc2 && *(c+1) == 0xa0)) ||
 			       *c == '_') c++;
 
-			// TODO: Add pedantic mode which checks whether the characters used
-			// in the identifier belong to the "191 graphic characters" defined
-			// by the standard.
+			/// \todo Add pedantic mode which checks whether the characters used
+			/// in the identifier belong to the "191 graphic characters" defined
+			/// by the standard.
 
 			if (*c == *bc) {
-				// TODO: Emit error indicating that this is a garbage character
-				// in the file.
+				/// \todo Emit error indicating that this is a garbage character
+				/// in the file.
 				c++;
 				emit(kTokenInvalid);
 			} else {
--- llhd/vhdl/Token.hpp
+++ llhd/vhdl/Token.hpp
@@ -1,38 +1,43 @@
 /* Copyright (c) 2014 Fabian Schuiki */
 #pragma once
+#include "llhd/SourceBuffer.hpp"
+#include "llhd/SourceLocation.hpp"
 #include "llhd/vhdl/TokenType.hpp"
 #include <ostream>
 
 namespace llhd {
 namespace vhdl {
 
-struct TokenPosition
-{
-	int line;
-	int column;
-	TokenPosition(): line(0), column(0) {}
+// struct TokenPosition
+// {
+// 	int line;
+// 	int column;
+// 	TokenPosition(): line(0), column(0) {}
+// };
+
+// struct TokenRange
+// {
+// 	TokenPosition start;
+// 	TokenPosition end;
+// 	TokenRange() {}
+// 	TokenRange(TokenPosition& s, TokenPosition& e): start(s), end(e) {}
+// };
+
+class Token {
+public:
+	SourceRange range;
+	unsigned type;
+
+	Token(const SourceRange r, unsigned t):
+		range(r),
+		type(t) {}
 };
 
-struct TokenRange
-{
-	TokenPosition start;
-	TokenPosition end;
-	TokenRange() {}
-	TokenRange(TokenPosition& s, TokenPosition& e): start(s), end(e) {}
-};
+// const char* tokenTypeToString(TokenType t);
 
-struct Token
-{
-	TokenType type;
-	std::string value;
-	TokenRange range;
-};
-
-const char* tokenTypeToString(TokenType t);
-
-std::ostream& operator<< (std::ostream& o, const TokenPosition& p);
-std::ostream& operator<< (std::ostream& o, const TokenRange& r);
-std::ostream& operator<< (std::ostream& o, const Token& tkn);
+// std::ostream& operator<< (std::ostream& o, const TokenPosition& p);
+// std::ostream& operator<< (std::ostream& o, const TokenRange& r);
+// std::ostream& operator<< (std::ostream& o, const Token& tkn);
 
 } // namespace vhdl
 } // namespace llhd
