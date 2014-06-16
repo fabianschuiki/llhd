7ee6278850750384c5e57ea991e534dba83c9d60
Fabian Schuiki <fabian@schuiki.ch>
2014-06-16 21:03:20 +0200

    tweaks to the console fmt

--- llhd/DiagnosticFormatterConsole.cpp
+++ llhd/DiagnosticFormatterConsole.cpp
@@ -10,19 +10,34 @@ DiagnosticFormatterConsole& DiagnosticFormatterConsole::operator<<(
 
 	for (unsigned i = 0; i < diag->getNumMessages(); i++) {
 		const DiagnosticMessage* msg = diag->getMessage(i);
+		std::string pad(i != 0 ? 2 : 0, ' ');
 
+		output << pad;
 		switch (msg->getType()) {
 			case kFatal: output << "\033[31;1mfatal error:\033[0m"; break;
-			case kError: output << "\033[31merror:\033[0m"; break;
+			case kError: output << "\033[31;1merror:\033[0m"; break;
 			case kWarning: output << "\033[33;1mwarning:\033[0m"; break;
 			case kNote: output << "\033[1mnote:\033[0m"; break;
 			case kFixit: output << "\033[36;1mfixit:\033[0m"; break;
 			default: output << "unspecified:"; break;
 		}
-		output << " ";
-		if (i == 0) output << "\033[1m";
-		output << msg->getMessage();
-		if (i == 0) output << "\033[0m";
+		output << " \033[1m";
+
+		const char* p = msg->getMessage();
+		unsigned line = 0;
+		while (*p != 0) {
+			if (*p == '\n') {
+				if (line++ == 0)
+					output << "\033[0m";
+				output << '\n' << pad << "  ";
+			} else {
+				output.put(*p);
+			}
+			p++;
+		}
+		// if (i == 0) output << "\033[1m";
+		// output << msg->getMessage();
+		// if (i == 0) output << "\033[0m";
 		output << '\n';
 
 		// output << "- message " << msg->getMessage() << '\n';
