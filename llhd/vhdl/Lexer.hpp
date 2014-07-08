67da40c755a37af7afab0b39bc7df8f98ca539ef
Fabian Schuiki <fabian@schuiki.ch>
2014-07-08 11:31:09 +0200

    added Lexer comment

--- llhd/vhdl/Lexer.hpp
+++ llhd/vhdl/Lexer.hpp
@@ -9,6 +9,10 @@ class TokenContext;
 
 namespace vhdl {
 
+/// Groups the bytes of a source file into individual tokens. See the TokenType
+/// enum for an overview over the types of tokens that are generated. The tokens
+/// are emitted into a TokenContext, and may later be passed to the Parser to be
+/// turned into an abstract syntax tree.
 class Lexer {
 	TokenContext& ctx;
 public:
