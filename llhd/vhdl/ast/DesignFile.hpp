af3e29581d68c206910e01cb271f88e10369b3f1
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-07-18 15:08:37 +0200

    tampering with AST layout

--- llhd/vhdl/ast/DesignFile.hpp
+++ llhd/vhdl/ast/DesignFile.hpp
@@ -1,21 +1,58 @@
 /* Copyright (c) 2014 Fabian Schuiki */
 #pragma once
+#include "llhd/SourceLocation.hpp"
+#include <memory>
 
 namespace llhd {
 namespace vhdl {
 namespace ast {
 
+#define one(type, name) std::unique_ptr<type> name;
+#define many(type, name) std::vector<std::unique_ptr<type>> name;
+
 struct Node {
-
+	SourceRange range;
 };
 
-struct DesignUnit {
-
+struct Identifier : public Node {
+	std::string value;
 };
 
-struct DesignFile {
-	DesignUnit** units;
+struct ContextItem;
+struct DesignFile;
+struct DesignUnit;
+struct LibraryClause;
+struct LibraryUnit;
+struct Prefix;
+struct SelectedNamed;
+struct Suffix;
+
+struct DesignFile : public Node {
+	many (DesignUnit, units)
 };
+
+struct DesignUnit : public Node {
+	many (ContextItem, contextItems)
+	one (LibraryUnit, libraryUnit)
+};
+
+struct ContextItem : public Node {};
+
+struct LibraryClause : public ContextItem {
+	std::vector<Identifier> names;
+};
+
+struct UseClause : public ContextItem {
+	many (SelectedName, names)
+};
+
+struct SelectedName : public Node {
+	one (Prefix, prefix)
+	one (Suffix, suffix)
+};
+
+#undef one
+#undef many
 
 } // namespace ast
 } // namespace vhdl
