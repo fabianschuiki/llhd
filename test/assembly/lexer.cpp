5d7918681ce364192209de7623262921bee9a5f0
Fabian Schuiki <fabian@schuiki.ch>
2015-09-09 10:17:59 +0200

    Improve assembly lexer unit test
    Extend the unit test of the assembly lexer to cover most areas of input
    tokens that could be encountered. Does not cover diagnostic messages
    yet, since the lexer does not emit them.

--- test/assembly/lexer.cpp
+++ test/assembly/lexer.cpp
@@ -1,20 +1,99 @@
 /* Copyright (c) 2015 Fabian Schuiki */
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "llhd/assembly/lexer.hpp"
 
-static const char *input =
-	"@global_name %local_name # some random comment\n"
-	"i1 l1 ls1\n";
+using namespace llhd;
 
-TEST_CASE("basic lexical analysis") {
-	using namespace llhd;
-	AssemblyLexer lex(make_range(input), SourceLocation(), nullptr);
 
+TEST_CASE("empty input") {
+	AssemblyLexer lex(make_range(""), SourceLocation(), nullptr);
+	REQUIRE(lex);
+	REQUIRE(lex.current_token() == TOKEN_SOF);
+	REQUIRE(lex.next().current_token() == TOKEN_EOF);
+	REQUIRE(lex.next().current_token() == TOKEN_EOF);
+	REQUIRE_FALSE(lex);
+}
+
+TEST_CASE("global names") {
+	AssemblyLexer lex(make_range("@hello @elaborate\\uC2\\uA9name @"), SourceLocation(), nullptr);
 	REQUIRE(lex);
 	REQUIRE(lex.current_token() == TOKEN_SOF);
 	REQUIRE(lex.next().current_token() == TOKEN_NAME_GLOBAL);
+	REQUIRE(lex.current_string() == "@hello");
+	REQUIRE(lex.current_range() == SourceRange(SourceId(),0,6));
+	REQUIRE(lex.next().current_token() == TOKEN_NAME_GLOBAL);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE_FALSE(lex);
+}
+
+TEST_CASE("local names") {
+	AssemblyLexer lex(make_range("%hello %elaborate\\uC2\\uA9name %"), SourceLocation(), nullptr);
+	REQUIRE(lex);
+	REQUIRE(lex.current_token() == TOKEN_SOF);
 	REQUIRE(lex.next().current_token() == TOKEN_NAME_LOCAL);
+	REQUIRE(lex.current_string() == "%hello");
+	REQUIRE(lex.current_range() == SourceRange(SourceId(),0,6));
+	REQUIRE(lex.next().current_token() == TOKEN_NAME_LOCAL);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE_FALSE(lex);
+}
+
+TEST_CASE("invalid names") {
+	{
+		AssemblyLexer lex(make_range("@a\\ #blah"), SourceLocation(), nullptr);
+		REQUIRE(lex);
+		REQUIRE(lex.current_token() == TOKEN_SOF);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE_FALSE(lex);
+	}
+	{
+		AssemblyLexer lex(make_range("@a\\uA #blah"), SourceLocation(), nullptr);
+		REQUIRE(lex);
+		REQUIRE(lex.current_token() == TOKEN_SOF);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE_FALSE(lex);
+	}
+}
+
+TEST_CASE("number literals") {
+	{
+		AssemblyLexer lex(make_range(
+			"i1'4 l1'A ls24'DEADBEEF\n" // binary number literals
+			"i2'd1023 l523'hDEADBEEF\n" // decimal and hexadecimal number literals
+			"i123'" // invalid number literal
+		), SourceLocation(), nullptr);
+		REQUIRE(lex);
+		REQUIRE(lex.current_token() == TOKEN_SOF);
+		REQUIRE(lex.next().current_token() == TOKEN_NUMBER_LITERAL);
+		REQUIRE(lex.next().current_token() == TOKEN_NUMBER_LITERAL);
+		REQUIRE(lex.next().current_token() == TOKEN_NUMBER_LITERAL);
+		REQUIRE(lex.next().current_token() == TOKEN_NUMBER_LITERAL);
+		REQUIRE(lex.next().current_token() == TOKEN_NUMBER_LITERAL);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE_FALSE(lex);
+	}
+	{
+		AssemblyLexer lex(make_range(
+			"i123' # trailing comment" // invalid number literal
+		), SourceLocation(), nullptr);
+		REQUIRE(lex);
+		REQUIRE(lex.current_token() == TOKEN_SOF);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+		REQUIRE_FALSE(lex);
+	}
+}
+
+TEST_CASE("types") {
+	AssemblyLexer lex(make_range("i1 l1 ls1 ;some comment\n"), SourceLocation(), nullptr);
+
+	REQUIRE(lex);
+	REQUIRE(lex.current_token() == TOKEN_SOF);
 	REQUIRE(lex.next().current_token() == TOKEN_TYPE);
 	REQUIRE(lex.next().current_token() == TOKEN_TYPE);
 	REQUIRE(lex.next().current_token() == TOKEN_TYPE);
@@ -22,3 +101,35 @@ TEST_CASE("basic lexical analysis") {
 	REQUIRE(lex.next().current_token() == TOKEN_EOF);
 	REQUIRE_FALSE(lex);
 }
+
+TEST_CASE("keywords") {
+	AssemblyLexer lex(make_range(
+		"mod proc func garbage ;some comment\n"
+	), SourceLocation(), nullptr);
+
+	REQUIRE(lex);
+	REQUIRE(lex.current_token() == TOKEN_SOF);
+	REQUIRE(lex.next().current_token() == TOKEN_KW_MOD);
+	REQUIRE(lex.next().current_token() == TOKEN_KW_PROC);
+	REQUIRE(lex.next().current_token() == TOKEN_KW_FUNC);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE(lex.next().current_token() == TOKEN_INVALID);
+	REQUIRE_FALSE(lex);
+}
+
+// TEST_CASE("basic lexical analysis") {
+// 	AssemblyLexer lex(make_range(
+// 		"@global_name %local_name  # some random comment\n" // names
+// 	), SourceLocation(), nullptr);
+
+// 	REQUIRE(lex);
+// 	REQUIRE(lex.current_token() == TOKEN_SOF);
+// 	REQUIRE(lex.next().current_token() == TOKEN_NAME_GLOBAL);
+// 	REQUIRE(lex.current_string() == "@global_name");
+// 	REQUIRE(lex.current_range() == SourceRange(SourceId(),0,12));
+// 	REQUIRE(lex.next().current_token() == TOKEN_NAME_LOCAL);
+// 	REQUIRE(lex.current_string() == "%local_name");
+// 	REQUIRE(lex.next().current_token() == TOKEN_EOF);
+// 	REQUIRE(lex.next().current_token() == TOKEN_EOF);
+// 	REQUIRE_FALSE(lex);
+// }
---
+++ test/main.cpp
@@ -0,0 +1,3 @@
+/* Copyright (c) 2015 Fabian Schuiki */
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
