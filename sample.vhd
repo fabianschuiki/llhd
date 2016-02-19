e07f9bda9bbbb317b7dc0c322762bffd5e352c73
Fabian Schuiki <fabian@schuiki.ch>
2016-02-19 14:18:51 +0100

    [rebase] sample code

--- sample.vhd
+++ sample.vhd
@@ -1,3 +1,4 @@
+-- alu
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -22,3 +23,34 @@ begin
 		end case;
 	end process;
 end rtl;
+
+
+-- testbench
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu_tb is
+end alu_tb;
+
+architecture tb of alu_tb is
+	signal a, b : std_logic_vector(7 downto 0);
+	signal op : std_logic_vector(1 downto 0);
+	signal r : std_logic_vector(7 downto 0);
+begin
+	dut: entity alu port map (
+		a => a,
+		b => b,
+		op => op,
+		r => r
+	);
+
+	p_stim: process begin
+		a <= "00010010";
+		b <= "00001010";
+		op <= "00"; wait for 1 ns; -- r = "00011100"
+		op <= "01"; wait for 1 ns; -- r = "00001000"
+		op <= "10"; wait for 1 ns; -- r = "00000010"
+		op <= "11"; wait for 1 ns; -- r = "00011010"
+	end process;
+end tb;
