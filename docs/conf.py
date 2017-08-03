c9dbcd802ec644e23d144561dae8f148b6419089
Fabian Schuiki <fabian@schuiki.ch>
2017-08-03 12:27:58 +0200

    add llhd pygments lexer
    Add a lexer for LLHD to the documentation's conf.py file. It's very
    basic for now, but makes things look a bit nicer.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = '0.1.0' # long version, including alpha/beta/rc tags
 master_doc = 'index'
 source_suffix = ['.rst', '.md']
 source_parsers = {
-    '.md': CommonMarkParser,
+	'.md': CommonMarkParser,
 }
 
 exclude_patterns = ['_build']
@@ -25,3 +25,34 @@ todo_include_todos = False
 
 # HTML output
 html_theme = 'sphinx_rtd_theme'
+
+
+# Define a custom pygments lexer for LLHD.
+import re
+from pygments.lexer import RegexLexer, words
+from pygments.token import Keyword, Name, Text, Comment, Operator, Number, Punctuation
+from sphinx.highlighting import lexers
+
+class LLHDLexer(RegexLexer):
+	name = 'llhd'
+	filenames = ['*.llhd']
+	flags = re.MULTILINE
+
+	tokens = {
+		'root': [
+			(r'\n', Text),
+			(r'\s+', Text),
+			(r';.*?$', Comment.Single),
+			(r'[=]', Operator),
+			(r'[()\[\]{},.]', Punctuation),
+			(r'[%@]([0-9a-zA-Z_.$]|\\[0-9a-fA-F]{2})+', Name),
+			(r'([0-9a-zA-Z_.$]|\\[0-9a-fA-F]{2})+:', Name.Label),
+			(r'[ilsn][0-9]+|void|metadata|label|time|\$|\*', Keyword.Type),
+			(words("const type func proc entity decl call inst wait br drive probe add sub mul div mod rem cmp alloc free var sig not and or xor now ret laod store".split(), suffix=r'\b'), Keyword),
+			(r'[0-9]+(\.[0-9]+)?([pnum]?s)', Number.Time),
+			(r'[0-9]+', Number.Integer),
+			(r'[a-zA-Z_]', Text),
+		]
+	}
+
+lexers['llhd'] = LLHDLexer()
