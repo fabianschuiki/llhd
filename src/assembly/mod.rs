acac1d34627d43e4eab6b8b3272fefd1df5a5ad6
Fabian Schuiki <fabian@schuiki.ch>
2019-01-14 09:52:15 +0100

    make assembly internal mods private
    Make the `assembly::reader` and `assembly::writer` modules private and
    export their contents as appropriate.

--- src/assembly/mod.rs
+++ src/assembly/mod.rs
@@ -3,8 +3,7 @@
 //! Facilities to emit a module as human-readable assembly, or to parse such
 //! assembly back into a module.
 
-pub mod reader;
-pub mod writer;
+mod reader;
+mod writer;
 
-pub use self::reader::parse_str;
-pub use self::writer::Writer;
+pub use self::{reader::parse_str, writer::Writer};
