93096167cdfb15a8c2f7ffdf678868d93219d2f4
Fabian Schuiki <fabian@schuiki.ch>
2019-01-15 08:54:22 +0100

    fix printing of time constants
    Format time constants with an SI prefix and decimal point as
    appropriate, rather than emitting them as a ratio. Also add a test case.
    Fixes #28.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 - Fix blocks with temporary names not being parsed properly.
 - Fix parsing of comparison operations.
+- Fix time constant printing. ([#28](https://github.com/fabianschuiki/llhd/issues/28))
 
 ## 0.2.0 - 2019-01-14
 ### Added
--- src/konst.rs
+++ src/konst.rs
@@ -2,11 +2,11 @@
 
 //! This module implements constant values.
 
+use crate::ty::*;
+use crate::value::*;
 use num::{BigInt, BigRational};
 use std;
 use std::sync::Arc;
-use crate::ty::*;
-use crate::value::*;
 
 pub type Const = Arc<ConstKind>;
 
@@ -145,7 +145,7 @@ impl std::fmt::Display for ConstTime {
         use num::Zero;
         let mut any = false;
         if !self.time.is_zero() || self.is_zero() {
-            write!(f, "{}s", self.time)?;
+            write_ratio_as_si(&self.time, f)?;
             any = true;
         }
         if !self.delta.is_zero() {
@@ -163,6 +163,42 @@ impl std::fmt::Display for ConstTime {
         }
         Ok(())
     }
+}
+
+fn write_ratio_as_si(ratio: &BigRational, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    use num::{One, Zero};
+    if ratio.is_zero() {
+        return write!(f, "0s");
+    }
+    let prefices = ["", "m", "u", "n", "p", "f", "a"];
+    let mut scaled = ratio.clone();
+    let mut prefix = 0;
+    let mut shift = 0;
+    while prefix + 1 < prefices.len() && shift < 9 {
+        if scaled >= One::one() {
+            if scaled.is_integer() {
+                break;
+            } else {
+                shift += 3;
+            }
+        } else {
+            prefix += 1;
+        }
+        scaled = scaled * BigRational::from_integer(BigInt::from(1000));
+    }
+    let rounded = format!("{}", scaled.round());
+    if shift > 0 {
+        write!(
+            f,
+            "{}.{}{}s",
+            &rounded[0..3],
+            &rounded[3..],
+            prefices[prefix]
+        )?;
+    } else {
+        write!(f, "{}{}s", rounded, prefices[prefix])?;
+    }
+    Ok(())
 }
 
 /// Create a new integer constant.
@@ -183,5 +219,61 @@ pub fn const_zero(ty: &Type) -> Const {
         IntType(sz) => const_int(sz, BigInt::zero()),
         TimeType => const_time(BigRational::zero(), BigInt::zero(), BigInt::zero()),
         ref x => panic!("no const zero value for type {}", x),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn time_formatting() {
+        let make = |num: usize, denom: usize, delta: usize, epsilon: usize| {
+            format!(
+                "{}",
+                ConstTime::new(
+                    BigRational::new(num.into(), denom.into()),
+                    delta.into(),
+                    epsilon.into()
+                )
+            )
+        };
+        assert_eq!(make(0, 1, 0, 0), "0s");
+        assert_eq!(make(0, 1, 0, 1), "1e");
+        assert_eq!(make(0, 1, 1, 0), "1d");
+        assert_eq!(make(0, 1, 1, 1), "1d 1e");
+
+        assert_eq!(make(1, 1, 0, 0), "1s");
+        assert_eq!(make(1, 1, 0, 1), "1s 1e");
+        assert_eq!(make(1, 1, 1, 0), "1s 1d");
+        assert_eq!(make(1, 1, 1, 1), "1s 1d 1e");
+
+        assert_eq!(make(1, 10, 0, 0), "100ms");
+        assert_eq!(make(1, 100, 0, 0), "10ms");
+        assert_eq!(make(1, 1000, 0, 0), "1ms");
+        assert_eq!(make(1, 10000, 0, 0), "100us");
+        assert_eq!(make(1, 100000, 0, 0), "10us");
+        assert_eq!(make(1, 1000000, 0, 0), "1us");
+        assert_eq!(make(1, 10000000, 0, 0), "100ns");
+        assert_eq!(make(1, 100000000, 0, 0), "10ns");
+        assert_eq!(make(1, 1000000000, 0, 0), "1ns");
+        assert_eq!(make(1, 10000000000, 0, 0), "100ps");
+        assert_eq!(make(1, 100000000000, 0, 0), "10ps");
+        assert_eq!(make(1, 1000000000000, 0, 0), "1ps");
+        assert_eq!(make(1, 10000000000000, 0, 0), "100fs");
+        assert_eq!(make(1, 100000000000000, 0, 0), "10fs");
+        assert_eq!(make(1, 1000000000000000, 0, 0), "1fs");
+        assert_eq!(make(1, 10000000000000000, 0, 0), "100as");
+        assert_eq!(make(1, 100000000000000000, 0, 0), "10as");
+        assert_eq!(make(1, 1000000000000000000, 0, 0), "1as");
+
+        assert_eq!(make(500, 1, 0, 0), "500s");
+        assert_eq!(make(50, 1, 0, 0), "50s");
+        assert_eq!(make(5, 1, 0, 0), "5s");
+        assert_eq!(make(5, 10, 0, 0), "500ms");
+        assert_eq!(make(5, 100, 0, 0), "50ms");
+        assert_eq!(make(5, 1000, 0, 0), "5ms");
+
+        assert_eq!(make(1, 3, 0, 0), "333.333333333ms");
     }
 }
