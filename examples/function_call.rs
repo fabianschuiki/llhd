0ca58722e9f6d1473bfe3ff361c827fd74b6ae32
Fabian Schuiki <fabian@schuiki.ch>
2019-04-22 16:44:37 +0200

    examples: add function call example
    Add an example that shows how functions can be called.

---
+++ examples/function_call.rs
@@ -0,0 +1,27 @@
+// Copyright (c) 2017-2019 Fabian Schuiki
+use llhd::ir::prelude::*;
+
+fn main() {
+    let name = UnitName::Global("foo".to_owned());
+    let mut sig = Signature::new();
+    sig.set_return_type(llhd::int_ty(32));
+    let mut func = Function::new(name, sig);
+    {
+        let mut builder = FunctionBuilder::new(&mut func);
+        let bb = builder.block();
+        builder.append_to(bb);
+        let mut sig = Signature::new();
+        sig.add_input(llhd::int_ty(1));
+        sig.add_input(llhd::int_ty(42));
+        sig.set_return_type(llhd::int_ty(32));
+        let ext = builder.add_extern(ExtUnitData {
+            name: UnitName::Global("bar".to_owned()),
+            sig,
+        });
+        let v1 = builder.ins().const_int(1, 0);
+        let v2 = builder.ins().const_int(42, 9001);
+        let v3 = builder.ins().call(ext, vec![v1, v2]);
+        builder.ins().ret_value(v3);
+    }
+    println!("{}", func.dump());
+}
