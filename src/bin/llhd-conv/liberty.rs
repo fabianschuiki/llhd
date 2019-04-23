ed4f58e01446f90c89c4dc0aea13287128bad32e
Fabian Schuiki <fabian@schuiki.ch>
2019-04-23 18:04:01 +0200

    llhd-conv: initial setup

---
+++ src/bin/llhd-conv/liberty.rs
@@ -0,0 +1,398 @@
+// Copyright (c) 2017-2019 Fabian Schuiki
+
+//! Lexer and parser for Liberty files.
+
+use llhd::{int_ty, ir::prelude::*, signal_ty};
+use std::collections::HashMap;
+
+/// A lexer for Liberty files.
+pub struct Lexer<I> {
+    input: I,
+    peek: [Option<u8>; 2],
+    done: bool,
+    offset: usize,
+    line: usize,
+    column: usize,
+}
+
+/// The token emitted by the lexer.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Ident(String),
+    Literal(String),
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Comma,
+    Colon,
+    Semicolon,
+}
+
+impl<I: Iterator<Item = std::io::Result<u8>>> Lexer<I> {
+    /// Create a new lexer.
+    pub fn new(input: I) -> Self {
+        let mut lexer = Self {
+            input,
+            peek: [None, None],
+            done: false,
+            offset: 0,
+            line: 0,
+            column: 0,
+        };
+        lexer.bump();
+        lexer.bump();
+        lexer.offset = 0;
+        lexer
+    }
+
+    /// Advance the lexer to the next character.
+    fn bump(&mut self) {
+        self.offset += 1;
+        if self.peek[0] == Some('\n' as u8) {
+            self.line += 1;
+            self.column = 0;
+        }
+        self.peek[0] = self.peek[1];
+        if !self.done {
+            self.peek[1] = self.input.next().map(|b| b.unwrap());
+            if self.peek[1].is_none() {
+                self.done = true;
+            }
+        }
+    }
+}
+
+fn should_skip(c: char) -> bool {
+    c.is_whitespace() || c == '\\'
+}
+
+fn is_ident(c: char) -> bool {
+    c.is_alphanumeric() || c == '_' || c == '.' || c == '-' || c == '+'
+}
+
+impl<I: Iterator<Item = std::io::Result<u8>>> Iterator for Lexer<I> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Token> {
+        loop {
+            let p0 = self.peek[0].map(|c| c as char);
+            let p1 = self.peek[1].map(|c| c as char);
+            match (p0, p1) {
+                // Skip multi-line comments.
+                (Some('/'), Some('*')) => {
+                    self.bump();
+                    self.bump();
+                    while self.peek[0].is_some()
+                        && (self.peek[0] != Some('*' as u8) || self.peek[1] != Some('/' as u8))
+                    {
+                        self.bump();
+                    }
+                    self.bump();
+                    self.bump();
+                    continue;
+                }
+                // Skip single-line comments.
+                (Some('/'), Some('/')) => {
+                    self.bump();
+                    self.bump();
+                    while self.peek[0].is_some() && self.peek[0] != Some('\n' as u8) {
+                        self.bump();
+                    }
+                    continue;
+                }
+                // Skip whitespace and escapes.
+                (Some(c), _) if should_skip(c) => {
+                    self.bump();
+                    continue;
+                }
+                // Parse symbols.
+                (Some('('), _) => {
+                    self.bump();
+                    return Some(Token::LParen);
+                }
+                (Some(')'), _) => {
+                    self.bump();
+                    return Some(Token::RParen);
+                }
+                (Some('{'), _) => {
+                    self.bump();
+                    return Some(Token::LBrace);
+                }
+                (Some('}'), _) => {
+                    self.bump();
+                    return Some(Token::RBrace);
+                }
+                (Some(','), _) => {
+                    self.bump();
+                    return Some(Token::Comma);
+                }
+                (Some(':'), _) => {
+                    self.bump();
+                    return Some(Token::Colon);
+                }
+                (Some(';'), _) => {
+                    self.bump();
+                    return Some(Token::Semicolon);
+                }
+                // Identifiers and numbers.
+                (Some(c), _) if is_ident(c) => {
+                    let mut v = vec![self.peek[0].unwrap()];
+                    self.bump();
+                    while let Some(c) = self.peek[0] {
+                        if !is_ident(c as char) {
+                            break;
+                        }
+                        v.push(c);
+                        self.bump();
+                    }
+                    let v = match String::from_utf8(v) {
+                        Ok(v) => v,
+                        Err(e) => panic!(
+                            "syntax error: line {} column {} (offset {}): invalid UTF-8 string; {}",
+                            self.line + 1,
+                            self.column + 1,
+                            self.offset,
+                            e
+                        ),
+                    };
+                    return Some(Token::Ident(v));
+                }
+                // Literals.
+                (Some('"'), _) => {
+                    self.bump();
+                    let mut v = vec![];
+                    while let Some(c) = self.peek[0] {
+                        if self.peek[0] == Some('"' as u8) {
+                            break;
+                        }
+                        if self.peek[0] == Some('\\' as u8) {
+                            self.bump();
+                        }
+                        v.push(c);
+                        self.bump();
+                    }
+                    self.bump();
+                    let v = match String::from_utf8(v) {
+                        Ok(v) => v,
+                        Err(e) => panic!(
+                            "syntax error: line {} column {} (offset {}): invalid UTF-8 string; {}",
+                            self.line + 1,
+                            self.column + 1,
+                            self.offset,
+                            e
+                        ),
+                    };
+                    return Some(Token::Literal(v));
+                }
+                // End of file.
+                (None, _) => return None,
+                (Some(c), _) => panic!(
+                    "syntax error: line {} column {} (offset {}): unexpected \"{}\"",
+                    self.line + 1,
+                    self.column + 1,
+                    self.offset,
+                    c
+                ),
+            }
+        }
+    }
+}
+
+/// A visitor for a Liberty file.
+pub trait Visitor {
+    /// Called for `name : value;` fields.
+    fn visit_scalar(&mut self, _name: String, _value: String) {}
+    /// Called for `name(values);` fields.
+    fn visit_array(&mut self, _name: String, _values: Vec<String>) {}
+    /// Called for `name(values) { ... }` fields.
+    fn visit_group_begin(&mut self, _name: String, _values: Vec<String>) {}
+    /// Called for end of groups.
+    fn visit_group_end(&mut self) {}
+}
+
+/// Parse a entire Liberty file.
+pub fn parse(p: &mut impl Iterator<Item = Token>, with: &mut impl Visitor) {
+    loop {
+        let name = match p.next() {
+            Some(Token::Ident(ident)) => ident,
+            Some(Token::RBrace) | None => return,
+            _ => panic!("syntax error: expected field name"),
+        };
+        match p.next() {
+            Some(Token::Colon) => {
+                let value = match p.next() {
+                    Some(Token::Ident(ident)) => ident,
+                    Some(Token::Literal(literal)) => literal,
+                    _ => panic!("syntax error: expected field value after \":\""),
+                };
+                match p.next() {
+                    Some(Token::Semicolon) => (),
+                    _ => panic!("syntax error: expected \";\" after field value"),
+                }
+                with.visit_scalar(name, value);
+            }
+            Some(Token::LParen) => {
+                let mut values = vec![];
+                loop {
+                    match p.next() {
+                        Some(Token::Ident(ident)) => values.push(ident),
+                        Some(Token::Literal(literal)) => values.push(literal),
+                        Some(Token::Comma) => (),
+                        Some(Token::RParen) => break,
+                        _ => panic!("syntax error: expected value or \")\""),
+                    }
+                }
+
+                match p.next() {
+                    Some(Token::Semicolon) => with.visit_array(name, values),
+                    Some(Token::LBrace) => {
+                        with.visit_group_begin(name, values);
+                        parse(p, with);
+                        with.visit_group_end();
+                    }
+                    _ => panic!("syntax error: expected \";\" or \"{\""),
+                }
+            }
+            _ => panic!("syntax error: expected \":\" or \"(\""),
+        }
+    }
+}
+
+/// The root visitor.
+pub struct RootVisitor<'a> {
+    module: &'a mut Module,
+    stack: Vec<Context>,
+    cell_name: Option<String>,
+    cell_inputs: Vec<String>,
+    cell_outputs: Vec<(String, String)>,
+    pin_name: Option<String>,
+    pin_function: Option<String>,
+    pin_direction: Option<String>,
+}
+
+enum Context {
+    None,
+    Cell,
+    Pin,
+}
+
+impl Visitor for RootVisitor<'_> {
+    fn visit_scalar(&mut self, name: String, value: String) {
+        match self.stack.last().unwrap() {
+            Context::Pin if name == "function" => self.pin_function = Some(value),
+            Context::Pin if name == "direction" => self.pin_direction = Some(value),
+            _ => (),
+        }
+    }
+
+    fn visit_group_begin(&mut self, name: String, mut values: Vec<String>) {
+        let context = match name.as_str() {
+            "cell" => {
+                self.cell_name = Some(values.pop().unwrap());
+                Context::Cell
+            }
+            "pin" => {
+                self.pin_name = Some(values.pop().unwrap());
+                self.pin_function = None;
+                self.pin_direction = None;
+                Context::Pin
+            }
+            _ => Context::None,
+        };
+        self.stack.push(context);
+    }
+
+    fn visit_group_end(&mut self) {
+        match self.stack.pop().expect("unbalanced LIB file") {
+            Context::Cell => self.emit_cell(),
+            Context::Pin => match self.pin_direction.take().unwrap().as_str() {
+                "input" => self.cell_inputs.push(self.pin_name.take().unwrap()),
+                "output" => self.cell_outputs.push((
+                    self.pin_name.take().unwrap(),
+                    self.pin_function.take().unwrap(),
+                )),
+                _ => (),
+            },
+            Context::None => (),
+        }
+    }
+}
+
+impl<'a> RootVisitor<'a> {
+    /// Create a new visitor.
+    pub fn new(module: &'a mut Module) -> Self {
+        Self {
+            module,
+            stack: Default::default(),
+            cell_name: Default::default(),
+            cell_inputs: Default::default(),
+            cell_outputs: Default::default(),
+            pin_name: Default::default(),
+            pin_function: Default::default(),
+            pin_direction: Default::default(),
+        }
+    }
+
+    fn emit_cell(&mut self) {
+        let mut sig = Signature::new();
+        let mut input_map = HashMap::new();
+        for name in self.cell_inputs.drain(..) {
+            let arg = sig.add_input(signal_ty(int_ty(1)));
+            input_map.insert(name, arg);
+        }
+        let mut funcs = vec![];
+        for (_name, func) in &self.cell_outputs {
+            let arg = sig.add_output(signal_ty(int_ty(1)));
+            funcs.push((arg, FunctionParser::new().parse(&func).unwrap()));
+        }
+        let name = UnitName::Global(self.cell_name.take().unwrap());
+        let mut ent = Entity::new(name, sig);
+        let mut builder = EntityBuilder::new(&mut ent);
+        for (arg, func) in funcs {
+            let arg = builder.unit().arg_value(arg);
+            let value = self.emit_term(&mut builder, &input_map, func);
+            builder.ins().con(arg, value);
+        }
+        self.module.add_entity(ent);
+        self.cell_outputs.clear();
+    }
+
+    fn emit_term(
+        &mut self,
+        builder: &mut EntityBuilder,
+        map: &HashMap<String, Arg>,
+        func: FunctionTerm,
+    ) -> Value {
+        match func {
+            FunctionTerm::Or(lhs, rhs) => {
+                let x = self.emit_term(builder, map, *lhs);
+                let y = self.emit_term(builder, map, *rhs);
+                builder.ins().or(x, y)
+            }
+            FunctionTerm::And(lhs, rhs) => {
+                let x = self.emit_term(builder, map, *lhs);
+                let y = self.emit_term(builder, map, *rhs);
+                builder.ins().and(x, y)
+            }
+            FunctionTerm::Not(term) => {
+                let x = self.emit_term(builder, map, *term);
+                builder.ins().not(x)
+            }
+            FunctionTerm::Atom(name) => builder.unit().arg_value(map[&name]),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FunctionTerm {
+    Or(Box<FunctionTerm>, Box<FunctionTerm>),
+    And(Box<FunctionTerm>, Box<FunctionTerm>),
+    Not(Box<FunctionTerm>),
+    Atom(String),
+}
+
+mod grammar {
+    include!(concat!(env!("OUT_DIR"), "/bin/llhd-conv/liberty.rs"));
+}
+
+use grammar::FunctionParser;
---
+++ src/bin/llhd-conv/main.rs
@@ -0,0 +1,148 @@
+// Copyright (c) 2017-2019 Fabian Schuiki
+
+#[macro_use]
+extern crate clap;
+use clap::Arg;
+use llhd::ir::Module;
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Read},
+    path::Path,
+};
+
+mod liberty;
+
+type Result<T> = std::result::Result<T, ()>;
+
+fn main() -> Result<()> {
+    // Parse the command line arguments.
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("input").required(true))
+        .arg(Arg::with_name("output").required(true))
+        .arg(
+            Arg::with_name("dump")
+                .long("--dump")
+                .help("Dump the intermediate LLHD"),
+        )
+        .get_matches();
+
+    let input_path = Path::new(matches.value_of("input").unwrap());
+    let output_path = Path::new(matches.value_of("output").unwrap());
+
+    // Detect input and output formats.
+    let (input_format, input_compression) = detect_format(input_path)?;
+    let (output_format, output_compression) = detect_format(output_path)?;
+
+    if input_compression != Compression::None {
+        eprintln!("input compression {} not supported", input_compression);
+        return Err(());
+    }
+    if output_compression != Compression::None {
+        eprintln!("output compression {} not supported", output_compression);
+        return Err(());
+    }
+
+    // Process the input.
+    let module = match input_format {
+        Format::Assembly => {
+            let mut input = File::open(input_path).map_err(|e| eprintln!("{}", e))?;
+            let mut contents = String::new();
+            input
+                .read_to_string(&mut contents)
+                .map_err(|e| eprintln!("{}", e))?;
+            llhd::assembly::parse_module(&contents).map_err(|e| eprintln!("{}", e))?
+        }
+        Format::Liberty => {
+            let input = File::open(input_path).map_err(|e| eprintln!("{}", e))?;
+            let input = BufReader::with_capacity(1 << 20, input);
+            let mut lexer = liberty::Lexer::new(input.bytes());
+            let mut module = Module::new();
+            let mut visitor = liberty::RootVisitor::new(&mut module);
+            liberty::parse(&mut lexer, &mut visitor);
+            module
+        }
+        f => panic!("{} inputs not supported", f),
+    };
+
+    // Dump the IR if requested.
+    if matches.is_present("dump") {
+        eprintln!("{}", module.dump());
+    }
+
+    // Generate the output.
+    match output_format {
+        Format::Assembly => {
+            let output = File::create(output_path).map_err(|e| eprintln!("{}", e))?;
+            let output = BufWriter::with_capacity(1 << 20, output);
+            llhd::assembly::write_module(output, &module);
+        }
+        f => panic!("{} inputs not supported", f),
+    }
+
+    Ok(())
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum Format {
+    Assembly,
+    Bitcode,
+    Verilog,
+    Vhdl,
+    Firrtl,
+    Edif,
+    Liberty,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum Compression {
+    None,
+    Gzip,
+}
+
+impl std::fmt::Display for Format {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Format::Assembly => write!(f, "LLHD assembly"),
+            Format::Bitcode => write!(f, "LLHD bitcode"),
+            Format::Verilog => write!(f, "Verilog netlist"),
+            Format::Vhdl => write!(f, "VHDL netlist"),
+            Format::Firrtl => write!(f, "FIRRTL"),
+            Format::Edif => write!(f, "EDIF netlist"),
+            Format::Liberty => write!(f, "LIB file"),
+        }
+    }
+}
+
+impl std::fmt::Display for Compression {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Compression::None => write!(f, "none"),
+            Compression::Gzip => write!(f, "gzip"),
+        }
+    }
+}
+
+fn detect_format(path: &Path) -> Result<(Format, Compression)> {
+    // Detect compression.
+    let (comp, path) = match path.extension().and_then(|e| e.to_str()) {
+        Some("gz") | Some("gzip") => (Compression::Gzip, path),
+        _ => (Compression::None, path),
+    };
+
+    // Detect format.
+    let fmt = match path.extension().and_then(|e| e.to_str()) {
+        Some("llhd") => Format::Assembly,
+        Some("bc") => Format::Bitcode,
+        Some("v") => Format::Verilog,
+        Some("vhdl") | Some("vhd") => Format::Vhdl,
+        Some("fir") => Format::Firrtl,
+        Some("edif") => Format::Edif,
+        Some("lib") => Format::Liberty,
+        _ => {
+            eprintln!("unknown file format {:?}", path);
+            return Err(());
+        }
+    };
+
+    Ok((fmt, comp))
+}
