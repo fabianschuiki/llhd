b08eda6dc2d6e1e5bb63b670fb9dcb3680020867
Fabian Schuiki <fabian@schuiki.ch>
2019-04-30 08:07:07 +0200

    ir: add RegMode to prelude

--- src/ir/prelude.rs
+++ src/ir/prelude.rs
@@ -4,5 +4,5 @@
 
 pub use crate::ir::{
     Arg, Block, Entity, EntityBuilder, Function, FunctionBuilder, Inst, ModUnit, Module, Opcode,
-    Process, ProcessBuilder, Signature, Unit, UnitBuilder, UnitName, Value,
+    Process, ProcessBuilder, RegMode, Signature, Unit, UnitBuilder, UnitName, Value,
 };
