93952331a29229a360bd8d38f5b2392cdad8f2f9
Fabian Schuiki <fabian@schuiki.ch>
2019-08-05 22:53:35 +0200

    tests: fix test failing verification

--- tests/assembly.rs
+++ tests/assembly.rs
@@ -83,11 +83,11 @@ fn process() {
 #[test]
 fn entity() {
     loopback! {"
-        entity @top (i32 %0) -> (i32 %1) {
-            %k0 = const i32 9000
-            %k1 = const i32 42
-            %2 = add i32 %0, %k0
-            %y = add i32 %0, %k1
+        entity @top (i32$ %0) -> (i32$ %1) {
+            %k0 = const i32$ 9000
+            %k1 = const i32$ 42
+            %2 = add i32$ %0, %k0
+            %y = add i32$ %0, %k1
         }
     "};
 }
